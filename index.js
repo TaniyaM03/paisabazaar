0cbcca395f33cbc7a568e430d3c8cb166447a16a
Taniya Maiti <taniyamaiti04@gmail.com>
2024-01-25 14:13:04 +0530

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+function storeinput(){
+    const inputarry=[];
+    const input1=document.getElementById("jj");
+    const input2=document.getElementById("kk");
+    const value1=input1.value;
+    const value2=input2.value;
+    inputarry.push(value1,value2);
+    console.log(inputarry);
+}
